4bc58c39aa0d673428c2eec72a198392111679ab
Serhii Khoma <srghma@gmail.com>
2024-12-15 16:50:28 +0700

    feat: update

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/registry/default/ui/button"
 
 export default function TestPage() {
   return (
